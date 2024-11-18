25f85fae05d51267922ea464f7293dadd461de6f
laichoun <aichouniasma@gmail.com>
2024-11-18 15:35:30 +0100

    WIP

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pibernar <@student.42Luxembourg.com>       +#+  +:+       +#+        */
+/*   By: laichoun <laichoun@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 16:14:17 by pibernar          #+#    #+#             */
-/*   Updated: 2024/11/15 16:15:33 by pibernar         ###   ########.fr       */
+/*   Updated: 2024/11/18 15:34:08 by laichoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,23 @@
 # include "../libft/libft.h"
 # include "../mlx/mlx.h"
 # include "../mlx/mlx_int.h"
+
+typedef struct s_file
+{
+	char	**cp_file;
+	char	**map;
+	int		so;
+	int		no;
+	int		ea;
+	int		we;
+	int		f;
+	int		c;
+	char	*tex_no;
+	char	*tex_so;
+	char	*tex_ea;
+	char	*tex_we;
+	int		f_rgb[3];
+	int		c_rgb[3];
+}			t_file;
 
 #endif
