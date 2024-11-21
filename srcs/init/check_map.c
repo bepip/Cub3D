cc88dd250593a48060f46d218083b60039dc62db
laichoun <aichouniasma@gmail.com>
2024-11-21 18:10:47 +0100

    WIP nbr of player

--- srcs/init/check_map.c
+++ srcs/init/check_map.c
@@ -6,7 +6,7 @@
 /*   By: laichoun <laichoun@student.42luxembourg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 17:53:01 by Asma              #+#    #+#             */
-/*   Updated: 2024/11/21 17:54:28 by laichoun         ###   ########.fr       */
+/*   Updated: 2024/11/21 18:10:20 by laichoun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,33 @@ int is_surrounded_walls(t_file *file)
     return (FAILURE);
 }
 
-/*
 int	is_valid_map(t_file *file)
 {
-    if (is_surrounded_walls(file))
-}
-*/
+    int i;
+    int j;
 
+    i = 0;
+    while (i < file->height)
+    {
+        j = 0;
+        while (j < file->width)
+        {
+            j = 0;
+            if (file->map[i][j] == 'N' || file->map[i][j] == 'S' || file->map[i][j] == 'W' || file->map[i][j] == 'E')
+            {
+                file->player ++;
+                j ++;
+            }
+            i ++;
+        }
+    }
+    if (file->player > 1)
+    {
+        printf("wrong number of player\n");
+        return (FAILURE);
+    }
+    return (SUCCESS);
+}
 
 int is_valid_row(t_file *file)
 {
@@ -42,42 +62,22 @@ int is_valid_row(t_file *file)
     int j;
 
     i = 0;
-//    ft_display_split(file->map);
-//    printf("height ; %d\n", file->height);
-//    printf("width ; %d\n", file->width);
     while (i < file->height)
     {
         j = 0;
         while (j < file->width)
         {
-//            printf("j : %d\n", j);
-//            printf("i : %d\n", i);
             while (j < file->width && file->map[i][j] == 32)
                 j ++;
-//            printf("j : %d\n", j);
-//            printf("i : %d\n", i);
-//            printf("char : %c\n", file->map[i][j]);
             if (j < file->width && file->map[i][j] != '1')
             {
-//                printf("j : %d\n", j);
-//                printf("i : %d\n", i);
-//                printf("char : %c\n", file->map[i][j]);
                 printf("NOK first char is not a wall border_rows, position i ; %d, j : %d\n", i, j);
                 return (FAILURE);
             }
-//            printf("j : %d\n", j);
-//            printf("i : %d\n", i);
-//            printf("char : %c\n", file->map[i][j]);
             while (j < file->width && file->map[i][j] != 32)
                 j ++;
-//            printf("j : %d\n", j);
-//            printf("i : %d\n", i);
-//            printf("char : %c\n", file->map[i][j]);
             if (file->map[i][j - 1] != '1' && file->map[i][j - 1] != ' ')
                 {
-//                    printf("j : %d\n", j);
-//                    printf("i : %d\n", i);
-//                    printf("char : %c\n", file->map[i][j]);
                     printf("NOK first char is not a wall border_rows test, position i ; %d, j : %d\n", i, j);
                     return (FAILURE);
                 }
@@ -94,50 +94,26 @@ int is_valid_col(t_file *file)
     int j;
 
     j = 1;
-//    printf("height col ; %d\n", file->height);
-//    printf("width col ; %d\n", file->width);
     while (j < file->width)
     {
         i = 0;
         while (i < file->height)
         {
-//            printf("j : %d\n", j);
-//            printf("i : %d\n", i);
             while (i < file->height && file->map[i][j] == 32)
                 i ++;
-//            printf("j : %d\n", j);
-//            printf("i : %d\n", i);
-//            if (i < file->height)
-//                printf("char : %c\n", file->map[i][j]);
             if (i < file->height && file->map[i][j] != '1')
             {
-//                printf("j : %d\n", j);
-//                printf("i : %d\n", i);
-//                printf("char : %c\n", file->map[i][j]);
                 printf("NOK first char is not a wall border_rows, position i ; %d, j : %d\n", i, j);
                 return (FAILURE);
             }
-//            printf("j : %d\n", j);
-//            printf("i : %d\n", i);
-//            if (i < file->height)
-//                printf("char : %c\n", file->map[i][j]);
             while (i < file->height && file->map[i][j] != 32)
                 i ++;
-//            printf("j : %d\n", j);
-//            printf("i : %d\n", i);
-//            if (i < file->height)
-//                printf("char : %c\n", file->map[i][j]);
             if (file->map[i - 1][j] != '1' && file->map[i - 1][j] != ' ')
             {
-//                    printf("j : %d\n", j);
-//                    printf("i : %d\n", i);
-//                    printf("char : %c\n", file->map[i - 1][j]);
                     printf("NOK first char is not a wall border_rows test, position i ; %d, j : %d\n", i -1, j);
                     return (FAILURE);
             }
             i ++;
-//          printf("j : %d\n", j);
-//            printf("i : %d\n", i);
         }
         j ++;
     }
